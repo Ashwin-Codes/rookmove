ec10875cc3921ca4f0814eb813d0711d0caab01b
Ashwin Bhargava <contact@ashwincodes.com>
2023-10-11 16:50:33 +0530

    fix: fix phaser not working with local uri of assets

--- src/scenes/ChessScene/preload.js
+++ src/scenes/ChessScene/preload.js
@@ -1,17 +1,10 @@
-import grid from "../../assets/grid.png"
-import rook from "../../assets/player-idle.png"
-import endpoint from "../../assets/end.png"
-import validMove from "../../assets/validmove.png"
-import playerAvatar from "../../assets/player-avatar.png"
-import opponentAvatar from "../../assets/opponent-avatar.png"
-
 export default function preload() {
-	this.load.image("player", rook)
-	this.load.image("endpoint", endpoint)
-	this.load.image("grid", grid)
-	this.load.image("validmove", validMove)
-	this.load.image("playeravatar", playerAvatar)
-	this.load.image("opponentavatar", opponentAvatar)
+	this.load.image("player", "/assets/player-idle.png")
+	this.load.image("endpoint", "/assets/end.png")
+	this.load.image("grid", "/assets/grid.png")
+	this.load.image("validmove", "/assets/validmove.png")
+	this.load.image("playeravatar", "/assets/player-avatar.png")
+	this.load.image("opponentavatar", "/assets/opponent-avatar.png")
 	this.load.plugin(
 		"rexcircularprogressplugin",
 		"https://raw.githubusercontent.com/rexrainbow/phaser3-rex-notes/master/dist/rexcircularprogressplugin.min.js",
